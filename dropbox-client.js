8119b8161c6eae7a86e89241b66a72c625f5df5b
thewritingdude <pippero_lo_smilzo@hotmail.com>
2023-08-07 17:33:54 +0200

    Progress

---
+++ dropbox-client.js
@@ -0,0 +1,96 @@
+var REDIRECT_URI = 'http://localhost:8080/index.html';
+var CLIENT_ID = 'dbabu374d69t7oz';
+var dbxAuth = new Dropbox.DropboxAuth({
+    clientId: CLIENT_ID,
+});
+var accessToken = window.localStorage.getItem("accessToken");
+
+// Parses the url and gets the access token if it is in the urls hash
+function getCodeFromUrl() {
+    return utils.parseQueryString(window.location.search).code;
+}
+
+// If the user was just redirected from authenticating, the urls hash will
+// contain the access token.
+function hasRedirectedFromAuth() {
+    return !!getCodeFromUrl();
+}
+
+function doAuth() {
+    dbxAuth.getAuthenticationUrl(REDIRECT_URI, undefined, 'code', 'offline', undefined, undefined, true)
+        .then(authUrl => {
+            window.sessionStorage.setItem("codeVerifier", dbxAuth.codeVerifier);
+            window.location.href = authUrl;
+        })
+        .catch((error) => console.error(error));
+};
+
+if (accessToken) {
+    dbxAuth.setAccessToken(accessToken);
+} else if (hasRedirectedFromAuth()) {
+    dbxAuth.setCodeVerifier(window.sessionStorage.getItem('codeVerifier'));
+    dbxAuth.getAccessTokenFromCode(REDIRECT_URI, getCodeFromUrl())
+        .then((response) => {
+            window.localStorage.setItem("accessToken", response.result.access_token);
+        })
+        .catch((error) => {
+            console.error(error.error || error);
+        });
+}
+
+function getGenowriteDropboxState() {
+    var accessToken = window.localStorage.getItem("accessToken");
+
+    if (!accessToken) {
+        console.error("No access token provided!");
+        return;
+    }
+    dbxAuth.setAccessToken(accessToken);
+    var dbx = new Dropbox.Dropbox({
+        auth: dbxAuth
+    });
+    return dbx.filesDownload({ path: '/status.json' })
+        .then(function (data) {
+            return data.result.fileBlob.text()
+        })
+        .catch(function (error) {
+            console.error(error.error || error);
+            window.localStorage.removeItem("accessToken");
+        });
+
+}
+
+function setGenowriteDropboxState(state) {
+    var accessToken = window.localStorage.getItem("accessToken");
+
+    if (!accessToken) {
+        console.error("No access token provided!");
+        return;
+    }
+    dbxAuth.setAccessToken(accessToken);
+    var dbx = new Dropbox.Dropbox({
+        auth: dbxAuth
+    });
+    fileState = new File([state], "status.json")
+    dbx.filesGetMetadata({ path: '/status.json' })
+        .then(function (response) {
+            console.log("got filesGetMetadata response:");
+            console.log(response);
+
+            // saving the current rev for the file:
+            var rev = response.result.rev;
+            return rev
+        })
+        .then(function (rev) {    
+            dbx.filesUpload({ path: '/status.json', contents: fileState, mode: { '.tag': 'update', 'update': rev } })
+                .then(function (data) {
+                    console.log("SEND STATUS OK")
+                    return data;
+                })
+                .catch(function (error) {
+                    console.error(error.error || error);
+                    window.localStorage.removeItem("accessToken");
+                });
+        })
+
+}
--- main.js
+++ main.js
@@ -5,19 +5,14 @@ var quill = new Quill('#editor', {
     theme: 'snow'
 },);
 
-if (localStorage.getItem('delta')) {
-    quill.setContents(JSON.parse(localStorage.getItem('delta')))    
-}
-
 
 /** Utils */
 function checkWordCount(text) {
-    if (!text) {
+    if (!text || text=="\n") {
         return;
     }
-    this.wordCount = text.match(/\w+/g).length;
+    this.wordCount = text.match(/\w+/g) ? text.match(/\w+/g).length : 0;
     this.characterCount = text.length;
-    console.log(this.wordCount, this.characterCount)
     document.querySelector("#word-count-span").innerHTML = this.wordCount
     document.querySelector("#character-count-span").innerHTML = this.characterCount
 }
@@ -27,22 +22,263 @@ function checkLineEndsWithStopOrSimilar(allText) {
     let lines = allText.split("\n")
     let charCount = 0
     for (let line of lines) {
-        if (line == ""){continue}
-        if (!["?", "!", "."].includes(line[line.length - 1])){
-            quill.formatText(charCount + line.length - 1, 1, 'background', '#ffcccb');   
+        if (line == "") { charCount += line.length + 1; continue }
+        if (!["?", "!", ".", "…"].includes(line[line.length - 1])){
+            quill.formatText(charCount + line.length - 1, 1, 'background', '#CD1818');
         }
         charCount += line.length + 1
     }
 }
 
+function checkIfStopOrSimilarIsPrecededBySpace(allText) {
+    for (let j in allText) {
+        i=parseInt(j)
+        if (["?", "!", ".", "…"].includes(allText[i]) && allText[i-1] == " "){
+            quill.formatText(i - 1, 1, 'background', '#CD1818');
+        }
+    }
+}
+
+function checkLineStartsWithNoSpace(allText) {
+    let lines = allText.split("\n")
+    let charCount = 0
+    for (let line of lines) {
+        if (line == "") { charCount += line.length + 1; continue }
+        if (line.charCodeAt(0)  == 160){
+            quill.formatText(charCount, 1, 'background', '#CD1818');   
+        }
+        charCount += line.length + 1
+    }
+}
+
+function replaceTokens(allText) {
+    for (token in tokenToCorrect) {
+        while (allText.indexOf(token) != -1) {
+            tokenIndex = allText.indexOf(token)
+            quill.deleteText(tokenIndex, token.length);
+            quill.insertText(tokenIndex, tokenToCorrect[token])
+            allText = quill.getText(0)
+        }
+    }
+}
+
+const tokenToCorrect =  {
+    '...': '…',
+    '\'': '’',
+    '--': '–',
+}
+
+function checkPeriodStartWithCapital(allText) {
+    let lines = allText.split("\n")
+    let charCount = 0
+    for (let line of lines) {
+        if (line == "") { charCount += line.length + 1; continue }
+        text = quill.getText(charCount, 1)
+        if (periodStartExceptions.includes(text)) {
+            realText = quill.getText(charCount + 2, 1)
+            if (realText != "" && realText != "\n" && realText != realText.toUpperCase()) {
+                quill.deleteText(charCount + 2, 1);
+                quill.insertText(charCount + 2, realText.toUpperCase())
+            }           
+
+        } else {
+            if (text != "" && text != "\n" && text != text.toUpperCase()) {
+                quill.deleteText(charCount, 1);
+                quill.insertText(charCount, text.toUpperCase())
+            }  
+            
+        }
+
+        secondaryCharCount = 0
+        let sentences = line.split(/\.|\?|\!|\…/)
+        for (let sentence of sentences) {
+            if (sentence == "") { secondaryCharCount += sentence.length + 1; continue }
+            
+            secondaryText = quill.getText(charCount + secondaryCharCount + 1, 1)
+            prevSecondaryText = quill.getText(charCount + secondaryCharCount, 1)
+            if (secondaryText != "" && secondaryText != "\n" && secondaryText != secondaryText.toUpperCase()
+                && secondaryCharCount + sentence.length <= line.length
+                && secondaryCharCount != 0
+            ) {
+                if (prevSecondaryText == " ") {
+                    quill.deleteText(charCount + secondaryCharCount + 1, 1);
+                    quill.insertText(charCount + secondaryCharCount + 1, secondaryText.toUpperCase())
+                } else {
+                    quill.formatText(charCount + secondaryCharCount, 1, 'background', '#CD1818');   
+                }
+               
+            }
+
+            secondaryCharCount += sentence.length + 1
+        }
+        
+        charCount += line.length + 1
+    }
+}
+
+const periodStartExceptions = ["–"]
+
+function checkNoDoubleSpaces(allText) {
+    for (let j in allText) {
+        i=parseInt(j)
+        if (allText[i] == " " && allText[i + 1] == " ") {
+            quill.formatText(i + 1, 1, 'background', '#FFD369');   
+        }
+    }
+}
+
+function checkNoTwoCapitalConsecutiveLetters(allText) {
+    for (let j in allText) {
+        i=parseInt(j)
+        if (allText[i].toLowerCase() != allText[i].toUpperCase() &&
+            allText[i + 1].toLowerCase() != allText[i + 1].toUpperCase() &&
+            allText[i + 2].toLowerCase() != allText[i + 2].toUpperCase() &&
+            allText[i] == allText[i].toUpperCase() &&
+            allText[i + 1] == allText[i + 1].toUpperCase() &&
+            allText[i + 2] != allText[i + 2].toUpperCase()) {
+            quill.formatText(i + 1, 1, 'background', '#FFD369');   
+        }
+    }
+}
+
 /** End utils */
+
+var timeout = null;
+var dropboxTimeout = null;
+var genowriteContent, genowriteKeys;
+if (accessToken) {
+    setTimeout(
+        ()=>syncGenowriteState(), 5
+    )
+} else if (localStorage.getItem('genowrite-content')) {
+    genowriteContent = JSON.parse(localStorage.getItem('genowrite-content'))
+    genowriteKeys = Object.keys(genowriteContent)
+    selectDocument(genowriteKeys[0])
+    refreshDocumentList(genowriteKeys)
+    allChecks()
+}
+
+var selectedDoc;
+
+function selectDocument(text) {
+    selectedDoc = text;
+    genowriteContent = JSON.parse(localStorage.getItem('genowrite-content'))
+    quill.setContents(genowriteContent[selectedDoc])
+    var modal = document.getElementById("menu-modal");
+    modal.style.display = "none";
+}
 
 quill.on('text-change', function (delta, oldDelta, source) {
     if (source == "user") {
-        const allText = quill.getText(0)
-        checkWordCount(allText)
-        checkLineEndsWithStopOrSimilar(allText)
-        localStorage.setItem('delta', JSON.stringify(quill.getContents()))
+
+        if (timeout) {
+            clearTimeout(timeout)
+            timeout = null
+        }
+        timeout = setTimeout(() => {
+            allChecks()
+            if (!genowriteContent) {
+                genowriteContent = {}
+            }
+            genowriteContent[selectedDoc] = quill.getContents()
+            localStorage.setItem('genowrite-content', JSON.stringify(genowriteContent))
+            timeout = null;
+        }, 150, selectedDoc)
+        
+        if (dropboxTimeout) {
+            clearTimeout(dropboxTimeout)
+            dropboxTimeout = null
+        }
+        dropboxTimeout = setTimeout(
+            () => uploadGenowriteState(JSON.stringify(genowriteContent)), 500
+        )
+
+        var range = quill.getSelection();
+        if (range && range.length == 0) {
+            quill.formatText(range.index - 2, 2, 'background', 'transparent');
+        }
+          
     }
 });
 
+function allChecks() {
+    const allText = quill.getText(0)
+
+    checkWordCount(allText)
+    checkLineEndsWithStopOrSimilar(allText)
+    checkLineStartsWithNoSpace(allText)
+    checkIfStopOrSimilarIsPrecededBySpace(allText)
+    checkPeriodStartWithCapital(allText)
+    checkNoDoubleSpaces(allText)
+    checkNoTwoCapitalConsecutiveLetters(allText)
+    replaceTokens(allText)
+}
+
+function addDocument() {
+    const name = document.querySelector("#newDocumentName").value
+    genowriteContent = JSON.parse(localStorage.getItem('genowrite-content'))
+    if (!genowriteContent) {
+        genowriteContent = {}
+    }
+    genowriteContent[name] = {}
+    genowriteKeys = Object.keys(genowriteContent)
+    localStorage.setItem('genowrite-content', JSON.stringify(genowriteContent))
+    refreshDocumentList(genowriteKeys)
+    document.querySelector("#newDocumentName").value = null
+}
+
+function refreshDocumentList(genowriteKeys) {
+    docsContainer = document.querySelector("#docs");
+    docsContainer.innerHTML = '';
+    for (let key of genowriteKeys) {
+        const fileTitle = document.createElement("p")
+        fileTitle.classList = "doc"
+        fileTitle.attributes["name"] = key
+        const textNode = document.createTextNode(key);
+        textNode.addEventListener('click', (ev)=>selectDocument(key))
+        fileTitle.append(textNode)
+        const deleteButton = document.createElement("button")
+        deleteButton.style.float = "right"
+        deleteButton.innerText = "Delete"
+        deleteButton.addEventListener('click', (ev)=>deleteDocument(key))
+        fileTitle.append(deleteButton)
+        docsContainer.appendChild(fileTitle)
+    }
+}
+
+function uploadGenowriteState(state) {
+    setGenowriteDropboxState(state);
+}
+
+function syncGenowriteState() {
+    for (let i = 4; i--; i >= 0){
+        let oldContent = localStorage.getItem('genowrite-content-'+i)
+        if (oldContent) {
+            localStorage.setItem('genowrite-content-'+(i+1), oldContent)
+        }
+    }
+    localStorage.setItem('genowrite-content-0', localStorage.getItem('genowrite-content'))
+    if (getGenowriteDropboxState) {
+        getGenowriteDropboxState().then((newState) => {
+            localStorage.setItem('genowrite-content', newState)
+            genowriteContent = JSON.parse(newState);
+            genowriteKeys = Object.keys(genowriteContent)
+            selectDocument(genowriteKeys[0])
+            refreshDocumentList(genowriteKeys)
+            allChecks()
+            document.querySelector("#dropbox-auth").style.display = "none";
+            document.querySelector("#dropbox-synced").style.display = "block";
+        })    
+    } else {
+        setTimeout(()=>syncGenowriteState(), 100)
+    }
+    
+}
+
+function deleteDocument(name) {
+    genowriteContent = JSON.parse(localStorage.getItem('genowrite-content'))
+    delete genowriteContent[name]
+    genowriteKeys = Object.keys(genowriteContent)
+    localStorage.setItem('genowrite-content', JSON.stringify(genowriteContent))
+    refreshDocumentList(genowriteKeys)
+}
